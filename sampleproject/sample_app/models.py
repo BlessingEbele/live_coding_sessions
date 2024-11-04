d438d13b1da0134649cbbe4f3c42b285f7bc41dc
blessingebele <blessmart.com@gmail.com>
2024-11-05 00:33:38 +0100

    commit :wq :qw :wq
    che__/settings.cpython-312.pyc
    :wq
    :qwi:wq

--- sampleproject/sample_app/models.py
+++ sampleproject/sample_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    first_name=models.CharField(max_lenght=10)
+    lastname=models.CharField(max_lenght=5)
+    adress =models.CharField(max_lenght=20)
+    bio =models.CharField(max_lenght=20)
+    is_validated =models.BooleanField(dafualt=True)
+    created =models.DateTimeField(auto_now_add=True)
